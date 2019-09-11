4818b5491d4f647e4d350481d43ac67aa1034e96
Tony Green <tgreen@albatrossflavour.com>
2019-09-11 21:34:48 +1000

    initial

---
+++ scripts/code_manager_config_version.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'json'
+
+environmentpath = ARGV[0]
+environment     = ARGV[1]
+
+r10k_deploy_file_path = File.join(environmentpath, environment, '.r10k-deploy.json')
+
+# output the sha1 from the control-repo
+puts JSON.parse(File.read(r10k_deploy_file_path))['signature']
